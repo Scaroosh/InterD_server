d59d693760b2486b137b58ddac53e8175ea6d9da
Vicky Wang <vicky.wang@epitech.eu>
2024-06-25 15:07:39 +0200

    [FIX] talib excluded

--- back/chat_bot/sidebar.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from datetime import date, timedelta
-
-# Sidebar Code
-def sidebar():
-    ticker = st.sidebar.selectbox('Select your Crypto', ["BTC-USD", "ETH-USD"])
-    start_date = st.sidebar.date_input('Start Date', date.today() - timedelta(days=365))
-    end_date = st.sidebar.date_input('End Date')
-    return ticker, start_date, end_date
