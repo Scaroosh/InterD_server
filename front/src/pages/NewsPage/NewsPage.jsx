8da3f899588a3dcd69bd39b6d7c400127a640c2b
Vicky Wang <vicky.wang@epitech.eu>
2024-06-26 11:16:39 +0200

    [ADD] news

--- front/src/pages/NewsPage/NewsPage.jsx
+++ front/src/pages/NewsPage/NewsPage.jsx
@@ -1,31 +1,115 @@
 import TemplatePage from "@/components/TemplatePage/TemplatePage.jsx";
-import classes from "./NewsPage.module.css"
-import newsPageImg from "@/assets/news_page_img.png"
-const NewsPage = () => {
-    return(
-        <TemplatePage>
-            <section className={classes.container}>
-                <div>
-                    <p className={classes.title}>We make the news.</p>
-                    <ul className={classes.article_info}>
-                        <li className={classes.article_date}>21 May</li>
-                        <li><p className={classes.article_text}>How much interest does your bank pay? - Trading Bot launches a broad interest campaign on TV, radio and billboard advertising in Germany's largest cities</p></li>
-                        <li className={classes.article_date}>28 May</li>
-                        <li><p className={classes.article_text}>Berlin based tights label saint sass and Trading Bot join forces on International Women's Day to raise awareness for the importance of female retirement savings.</p></li>
-                        <li className={classes.article_date}>04 June</li>
-                        <li><p className={classes.article_text}>Trading Bot celebrates its 5th birthday with 4 million customers and introduces a new card with a 1 percent Saveback reward for every card payment.</p></li>
-                    </ul>
-                </div>
-                <div className={classes.contact_info}>
-                    <img src={newsPageImg} alt="newsPageImg"/>
-                    <div className={classes.contact_text_container}>
-                            <p className={classes.contact}>Contacts</p>
-                            <p className={classes.contact_text}>24/7 Customer Support Trading bot is there for you every step of the way. We offer 24/7 support, allowing our clients to contact with any of our friendly and experienced customer service agents throughout the trading week. Whether you have questions or you would just like to provide us with feedback regarding our services, we’re here for you.</p>
-                    </div>
-                </div>
-            </section>
-        </TemplatePage>
-    )
-}
+import classes from "./NewsPage.module.css";
+import { useEffect, useState } from "react";
 
-export default NewsPage
+const NewsPage = () => {
+  const [ticker, setTicker] = useState("");
+  const [source, setSource] = useState("YahooFinance");
+  const [news, setNews] = useState([]);
+  const [modalContent, setModalContent] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchData = async (event) => {
+    event.preventDefault();
+    const url = import.meta.env.VITE_SERVER_URL + "news/get_news";
+    const requestBody = {
+      ticker: ticker,
+      source: source,
+      max_articles: 5,
+    };
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(requestBody),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setNews(data.articles);
+    } catch (error) {
+      console.error("Problem with fetch operation:", error);
+    }
+  };
+
+  const openModal = (article) => {
+    setModalContent(article);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleOverlayClick = (e) => {
+    if (e.target.className.includes("modalOverlay")) {
+      closeModal();
+    }
+  };
+
+  return (
+    <TemplatePage>
+      <div className={classes.container}>
+        <h1>News</h1>
+        <form onSubmit={fetchData}>
+          <input
+            type="text"
+            className={classes.searchBar}
+            value={ticker}
+            onChange={(e) => setTicker(e.target.value)}
+            placeholder="Enter Ticker"
+            required
+          />
+          <select
+            value={source}
+            onChange={(e) => setSource(e.target.value)}
+            className={classes.sourceSelect}
+          >
+            <option value="YahooFinance">Yahoo Finance</option>
+            <option value="CryptoNews">Crypto News</option>
+          </select>
+          <button type="submit">Get News</button>
+        </form>
+        <div className={classes.cardsContainer}>
+          {news.map((article, index) => (
+            <div
+              key={index}
+              className={classes.card}
+              onClick={() => openModal(article)}
+            >
+              <div className={classes.header}>{article.title}</div>
+            </div>
+          ))}
+        </div>
+        {isModalOpen && modalContent && (
+          <div className={classes.modalOverlay} onClick={handleOverlayClick}>
+            <div className={classes.modal} onClick={(e) => e.stopPropagation()}>
+              <button onClick={closeModal} className={classes.closeButton}>
+                ✖
+              </button>
+              <div className={classes.modalContent}>
+                <h2>{modalContent.title}</h2>
+                <p>{modalContent.text}</p>
+                <p>Sentiment: {modalContent.sentiment}</p>
+                <p>Polarity: {modalContent.sentiment_polarity.toFixed(3)}</p>
+                <a
+                  href={modalContent.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  source reference
+                </a>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </TemplatePage>
+  );
+};
+
+export default NewsPage;
