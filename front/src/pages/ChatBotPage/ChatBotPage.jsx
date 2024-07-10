97cad9951ddad5bfd65905ea92b4aaddfccb0fb3
Vicky Wang <vicky.wang@epitech.eu>
2024-07-10 10:30:38 +0200

    [FIX] Front

--- front/src/pages/ChatBotPage/ChatBotPage.jsx
+++ front/src/pages/ChatBotPage/ChatBotPage.jsx
@@ -1,7 +1,7 @@
 import classes from "./ChatBotPage.module.css";
 import TemplatePage from "@/components/TemplatePage/TemplatePage.jsx";
 import CryptocurrencySearchOutput from "@/components/ChatBotPageComponents/CryptocurrencySearchOutput/CryptocurrencySearchOutput.jsx";
-import ChatBotActionButtons from "@/components/ChatBotPageComponents/ChatBotActionButtons/ChatBotActionButtons.jsx";
+import ChatBotActionButtons from "@/components/ChatBotPageComponents/ChatBotPageComponents/ChatBotActionButtons/ChatBotActionButtons.jsx";
 import ChatBotChart from "@/components/ChatBotPageComponents/ChatBotPageComponents/ChatBotChart/ChatBotChart.jsx";
 
 const ChatBotPage = () => {
