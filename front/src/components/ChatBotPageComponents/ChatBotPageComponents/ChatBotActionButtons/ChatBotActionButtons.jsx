dd8c869831728bdfe4ffd8386ed31ff0c734cef2
Vicky Wang <vicky.wang@epitech.eu>
2024-07-10 10:20:32 +0200

    [FIX] API Calls to server

--- front/src/components/ChatBotPageComponents/ChatBotPageComponents/ChatBotActionButtons/ChatBotActionButtons.jsx
+++ front/src/components/ChatBotPageComponents/ChatBotPageComponents/ChatBotActionButtons/ChatBotActionButtons.jsx
@@ -1,187 +1,217 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import classes from './ChatBotActionButtons.module.css';
+import React, { useState } from "react";
+import axios from "axios";
+import classes from "./ChatBotActionButtons.module.css";
 
 const ChatBotActionButtons = () => {
-    const [response, setResponse] = useState(null);
-    const [error, setError] = useState(null);
-    const [formData, setFormData] = useState({
-        cryptocurrency: '',
-        tradeAmount: '',
-        tradingStrategy: ''
-    });
-    const [botId, setBotId] = useState(null); // State to store bot_id
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({
+    cryptocurrency: "",
+    tradeAmount: "",
+    tradingStrategy: "",
+  });
+  const [botId, setBotId] = useState(null); // State to store bot_id
 
-    const handleError = (err) => {
-        if (err.response) {
-            setError(`Error: ${err.response.status} ${err.response.statusText} - ${err.response.data}`);
-        } else if (err.request) {
-            setError('Error: No response received from server');
-        } else {
-            setError(`Error: ${err.message}`);
+  const handleError = (err) => {
+    if (err.response) {
+      setError(
+        `Error: ${err.response.status} ${err.response.statusText} - ${err.response.data}`
+      );
+    } else if (err.request) {
+      setError("Error: No response received from server");
+    } else {
+      setError(`Error: ${err.message}`);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError(null);
+
+    try {
+      const res = await axios.post(
+        import.meta.env.VITE_SERVER_URL + "bot/create_bot",
+        {
+          owner_id: "6684b8c13a116176f4064ddd",
+          ticker: formData.cryptocurrency,
+          usdt_amount: formData.tradeAmount,
         }
-    };
+      );
+      const botData = res.data;
+      if (botData && botData.bot_id) {
+        setBotId(botData.bot_id); // Save bot_id in state
+        setResponse("Bot created successfully");
+      } else {
+        setError("Failed to create bot");
+      }
+    } catch (err) {
+      handleError(err);
+    }
+  };
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const startBot = async () => {
+    if (!botId) {
+      setError("Bot is not created yet");
+      return;
+    }
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setError(null);
-
-        try {
-            const res = await axios.post("http://127.0.0.1:8080/bot/create_bot", {
-                owner_id: "6684b8c13a116176f4064ddd",
-                ticker: formData.cryptocurrency,
-                usdt_amount: formData.tradeAmount
-            });
-            const botData = res.data;
-            if (botData && botData.bot_id) {
-                setBotId(botData.bot_id); // Save bot_id in state
-                setResponse("Bot created successfully");
-            } else {
-                setError("Failed to create bot");
-            }
-        } catch (err) {
-            handleError(err);
+    try {
+      const res = await axios.post(
+        import.meta.env.VITE_SERVER_URL + "bot/start_bot",
+        {
+          bot_id: botId,
         }
-    };
+      );
+      setResponse(res.data);
+    } catch (err) {
+      handleError(err);
+    }
+  };
 
-    const startBot = async () => {
-        if (!botId) {
-            setError("Bot is not created yet");
-            return;
+  const stopBot = async () => {
+    if (!botId) {
+      setError("Bot is not created yet");
+      return;
+    }
+
+    try {
+      const res = await axios.post(
+        import.meta.env.VITE_SERVER_URL + "bot/stop_bot",
+        {
+          bot_id: botId,
+          ticker: formData.cryptocurrency,
         }
+      );
+      setResponse(res.data);
+    } catch (err) {
+      handleError(err);
+    }
+  };
 
-        try {
-            const res = await axios.post("http://127.0.0.1:8080/bot/start_bot", {
-                bot_id: botId
-            });
-            setResponse(res.data);
-        } catch (err) {
-            handleError(err);
+  const pauseBot = async () => {
+    if (!botId) {
+      setError("Bot is not created yet");
+      return;
+    }
+
+    try {
+      const res = await axios.post(
+        import.meta.env.VITE_SERVER_URL + "bot/pause_bot",
+        {
+          bot_id: botId,
+          ticker: formData.cryptocurrency,
         }
-    };
+      );
+      setResponse(res.data);
+    } catch (err) {
+      handleError(err);
+    }
+  };
 
-    const stopBot = async () => {
-        if (!botId) {
-            setError("Bot is not created yet");
-            return;
+  const resumeBot = async () => {
+    if (!botId) {
+      setError("Bot is not created yet");
+      return;
+    }
+
+    try {
+      const res = await axios.post(
+        import.meta.env.VITE_SERVER_URL + "bot/resume_bot",
+        {
+          bot_id: botId,
+          ticker: formData.cryptocurrency,
         }
+      );
+      setResponse(res.data);
+    } catch (err) {
+      handleError(err);
+    }
+  };
 
-        try {
-            const res = await axios.post("http://127.0.0.1:8080/bot/stop_bot", {
-                bot_id: botId,
-                ticker: formData.cryptocurrency
-            });
-            setResponse(res.data);
-        } catch (err) {
-            handleError(err);
+  const deleteBot = async () => {
+    if (!botId) {
+      setError("Bot is not created yet");
+      return;
+    }
+
+    try {
+      const res = await axios.post(
+        import.meta.env.VITE_SERVER_URL + "bot/delete_bot",
+        {
+          bot_id: botId,
         }
-    };
+      );
+      setResponse(res.data);
+    } catch (err) {
+      handleError(err);
+    }
+  };
 
-    const pauseBot = async () => {
-        if (!botId) {
-            setError("Bot is not created yet");
-            return;
-        }
-
-        try {
-            const res = await axios.post("http://127.0.0.1:8080/bot/pause_bot", {
-                bot_id: botId,
-                ticker: formData.cryptocurrency
-            });
-            setResponse(res.data);
-        } catch (err) {
-            handleError(err);
-        }
-    };
-
-    const resumeBot = async () => {
-        if (!botId) {
-            setError("Bot is not created yet");
-            return;
-        }
-
-        try {
-            const res = await axios.post("http://127.0.0.1:8080/bot/resume_bot", {
-                bot_id: botId,
-                ticker: formData.cryptocurrency
-            });
-            setResponse(res.data);
-        } catch (err) {
-            handleError(err);
-        }
-    };
-
-    const deleteBot = async () => {
-        if (!botId) {
-            setError("Bot is not created yet");
-            return;
-        }
-
-        try {
-            const res = await axios.post("http://127.0.0.1:8080/bot/delete_bot", {
-                bot_id: botId,
-            });
-            setResponse(res.data);
-        } catch (err) {
-            handleError(err);
-        }
-    };
-
-    return (
+  return (
+    <div>
+      <form className={classes.form} onSubmit={handleSubmit}>
         <div>
-            <form className={classes.form} onSubmit={handleSubmit}>
-                <div>
-                    <label>Cryptocurrency:</label>
-                    <input
-                        type="text"
-                        name="cryptocurrency"
-                        value={formData.cryptocurrency}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label>Trade Amount:</label>
-                    <input
-                        type="number"
-                        name="tradeAmount"
-                        value={formData.tradeAmount}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label>Trading Strategy:</label>
-                    <select
-                        name="tradingStrategy"
-                        value={formData.tradingStrategy}
-                        onChange={handleInputChange}
-                    >
-                        <option value="">Select strategy...</option>
-                        <option value="strategy1">Strategy 1</option>
-                        <option value="strategy2">Strategy 2</option>
-                    </select>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-            <div className={classes.container}>
-                <button className={classes.buttons} onClick={startBot}>start</button>
-                <button className={classes.buttons} onClick={stopBot}>stop</button>
-                <button className={classes.buttons} onClick={pauseBot}>pause</button>
-                <button className={classes.buttons} onClick={resumeBot}>resume</button>
-                <button className={classes.buttons} onClick={deleteBot}>delete</button>
-            </div>
-            {response && (
-                <div className={classes.response}>
-                    <div>Response: {JSON.stringify(response)}</div>
-                </div>
-            )}
-            {error && <div className={classes.error}>Error: {error}</div>}
+          <label>Cryptocurrency:</label>
+          <input
+            type="text"
+            name="cryptocurrency"
+            value={formData.cryptocurrency}
+            onChange={handleInputChange}
+          />
         </div>
-    );
+        <div>
+          <label>Trade Amount:</label>
+          <input
+            type="number"
+            name="tradeAmount"
+            value={formData.tradeAmount}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label>Trading Strategy:</label>
+          <select
+            name="tradingStrategy"
+            value={formData.tradingStrategy}
+            onChange={handleInputChange}
+          >
+            <option value="">Select strategy...</option>
+            <option value="strategy1">Strategy 1</option>
+            <option value="strategy2">Strategy 2</option>
+          </select>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div className={classes.container}>
+        <button className={classes.buttons} onClick={startBot}>
+          start
+        </button>
+        <button className={classes.buttons} onClick={stopBot}>
+          stop
+        </button>
+        <button className={classes.buttons} onClick={pauseBot}>
+          pause
+        </button>
+        <button className={classes.buttons} onClick={resumeBot}>
+          resume
+        </button>
+        <button className={classes.buttons} onClick={deleteBot}>
+          delete
+        </button>
+      </div>
+      {response && (
+        <div className={classes.response}>
+          <div>Response: {JSON.stringify(response)}</div>
+        </div>
+      )}
+      {error && <div className={classes.error}>Error: {error}</div>}
+    </div>
+  );
 };
 
 export default ChatBotActionButtons;
--- front/src/components/ChatBotPageComponents/ChatBotPageComponents/CryptocurrencySearchOutput/CryptocurrencySearchOutput.jsx
+++ front/src/components/ChatBotPageComponents/ChatBotPageComponents/CryptocurrencySearchOutput/CryptocurrencySearchOutput.jsx
@@ -1,59 +1,61 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import JSONPretty from 'react-json-pretty';
-import 'react-json-pretty/themes/monikai.css'; // подключаем тему для react-json-pretty
-import classes from './CryptocurrencySearchOutput.module.css';
+import React, { useState } from "react";
+import axios from "axios";
+import JSONPretty from "react-json-pretty";
+import "react-json-pretty/themes/monikai.css"; // подключаем тему для react-json-pretty
+import classes from "./CryptocurrencySearchOutput.module.css";
 
 const CryptocurrencySearchOutput = () => {
-    const [cryptoSymbol, setCryptoSymbol] = useState('');
-    const [result, setResult] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+  const [cryptoSymbol, setCryptoSymbol] = useState("");
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError(null);
-        setLoading(true);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    setLoading(true);
 
-        try {
-            const response = await axios.post('http://127.0.0.1:8080/crypto/analyze', { crypto_symbol: cryptoSymbol });
-            setResult(response.data);
-        } catch (err) {
-            setError('Error fetching data. Please try again.');
-        } finally {
-            setLoading(false);
-        }
-    };
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_SERVER_URL + "crypto/analyze",
+        { crypto_symbol: cryptoSymbol }
+      );
+      setResult(response.data);
+    } catch (err) {
+      setError("Error fetching data. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <div className={classes.container}>
-            <form onSubmit={handleSubmit} className={classes.form}>
-                <input
-                    type="text"
-                    className={classes.input}
-                    placeholder="Cryptocurrency Symbol"
-                    value={cryptoSymbol}
-                    onChange={(e) => setCryptoSymbol(e.target.value)}
-                    required
-                />
-                <button type="submit" className={classes.button}>Analyze</button>
-            </form>
-            {loading && (
-                <div className={classes.loading}>Loading...</div>
-            )}
-            {error && (
-                <div className={classes.alert}>
-                    {error}
-                </div>
-            )}
-            {result && !loading && (
-                <div className={classes.result}>
-                    <h6>Analysis Result</h6>
-                    <JSONPretty data={result} style={{ whiteSpace: 'pre-wrap' }}></JSONPretty>
-                </div>
-            )}
+  return (
+    <div className={classes.container}>
+      <form onSubmit={handleSubmit} className={classes.form}>
+        <input
+          type="text"
+          className={classes.input}
+          placeholder="Cryptocurrency Symbol"
+          value={cryptoSymbol}
+          onChange={(e) => setCryptoSymbol(e.target.value)}
+          required
+        />
+        <button type="submit" className={classes.button}>
+          Analyze
+        </button>
+      </form>
+      {loading && <div className={classes.loading}>Loading...</div>}
+      {error && <div className={classes.alert}>{error}</div>}
+      {result && !loading && (
+        <div className={classes.result}>
+          <h6>Analysis Result</h6>
+          <JSONPretty
+            data={result}
+            style={{ whiteSpace: "pre-wrap" }}
+          ></JSONPretty>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default CryptocurrencySearchOutput;
